b7259cfa66150dda02e87e1ead0876179c63e647
Steven Maitre <steven.maitre@gmail.com>
2025-01-16 15:49:47 +0100

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>HomePage</h1>
+    </div>
+  );
+};
+
+export default Home;
